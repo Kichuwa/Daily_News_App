8acbce08cbe08a6990e5d9453733a29aa81cdd20
Darren Soulier <56926037+Kichuwa@users.noreply.github.com>
2024-04-19 18:53:09 -0500

    Fixed handling of Article titles that declare None instead of String.

--- main.py
+++ main.py
@@ -16,9 +16,10 @@ news_descriptions = []
 article_count = 0
 
 for article in content["articles"]:
-    news_titles.append(article["title"])
-    news_descriptions.append(article["description"])
-    article_count = article_count + 1
+    if article["title"] is not None:
+        news_titles.append(article["title"])
+        news_descriptions.append(article["description"])
+        article_count = article_count + 1
 
 raw_message = ""
 index = 0
@@ -26,8 +27,10 @@ while index < 10:
     raw_message += str(news_titles[index]) + "\n" + str(news_descriptions[index]) + "\n\n"
     index = index + 1
 
+# Remove raw message post testing
 print(raw_message)
 
+# the encode removes the issue that arrives from various ascii chars that may appear.
 email_output = f"""\
 Subject: Your Daily MS News!
 
