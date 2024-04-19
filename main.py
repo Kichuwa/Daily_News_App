0ee78bb2b0c24c149abeb3cc25cb5a40800c1294
Darren Soulier <56926037+Kichuwa@users.noreply.github.com>
2024-04-18 22:01:23 -0500

    Formatted test API data for automated email services.

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import os
 import requests
 
 load_dotenv(".env")
-API_BASE_URL = "https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey="
+API_BASE_URL = "https://newsapi.org/v2/everything?q=microsoft&sortBy=publishedAt&apiKey="
 KEY = os.getenv("API_KEY")
 REQUEST_URL = API_BASE_URL + KEY
 
 req = requests.get(REQUEST_URL)
-content = req.text
+content = req.json()
 
-print(content)
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
+
+
